63abbf8f1b9c0ce68f7d9745fe382d7fa4043a19
Sarvjeet Maurya <sarvjeetm273303@gmail.com>
2024-06-29 06:00:31 +0530

    add sothing new

---
+++ day4_programe/banker_raulette.py
@@ -0,0 +1,5 @@
+names = ["Sarvjeet","Abhishek","Archana","Ritesh","Rambhavan","Rambhavati"]
+import random
+name_len = len(names)
+random_index = random.randint(0,name_len-1)
+print(names[random_index],"is pay the bill today")
---
+++ day4_programe/heads_or_tails.py
@@ -0,0 +1,6 @@
+import random
+random_score = random.randint(0,1)
+if random_score==1:
+    print("Heads")
+else:
+    print("Tails")
